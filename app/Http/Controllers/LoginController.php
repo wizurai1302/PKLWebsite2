e0b3c5ed94abec1bd05915f41c05107a59e1deb9
wizurai1302 <midhamk1302@gmail.com>
2023-07-31 09:16:05 +0700

    Fix(LoginController)PHP Doc

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 class LoginController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display the login form.
      *
      * @return \Illuminate\Http\Response
      */
@@ -16,36 +16,52 @@ class LoginController extends Controller
     {
         return view('login.login');
     }
+
+    /**
+     * Authenticate the user and log them in.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
     public function authenticate(Request $request)
     {
+        // Validate the user input (email and password)
         $login = $request->validate([
             'email' => 'required|email:dns',
             'password' => 'required'
         ]);
 
+        // Attempt to authenticate the user with provided credentials
         if (Auth::attempt($login)) {
+            // If authentication is successful, regenerate the session to prevent session fixation attacks
             $request->session()->regenerate();
 
+            // Redirect the user to the intended URL (usually '/home') after successful login
             return redirect()->intended('/home');
         }
 
+        // If authentication fails, redirect back to the login form with an error message
         return back()->with('LoginError', 'Gagal Login');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Logout the currently authenticated user.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function logout(Request $request)
     {
+        // Log out the currently authenticated user
         Auth::logout();
     
+        // Invalidate the user's session
         $request->session()->invalidate();
         
+        // Regenerate a new CSRF token for the session to prevent CSRF attacks
         $request->session()->regenerateToken();
         
+        // Redirect the user to the home page after successful logout
         return redirect('/');
     }
 }
